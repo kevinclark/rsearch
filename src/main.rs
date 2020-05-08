e1f588a152b4dce04fcfacc09e1b55d558479ef6
Kevin Clark <kevin.clark@gmail.com>
2020-05-08 15:36:31 -0700

    Arg parsing - but no args yet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+use clap::{crate_authors, crate_description, crate_name, crate_version, App};
 
 fn main() {
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .about(crate_description!())
+        .author(crate_authors!())
+    .get_matches();
 }
