2aced9f605860430f4509d802e40a8f32dc0106b
Kevin Clark <kevin.clark@gmail.com>
2020-11-14 00:19:50 -0800

    Fix tests and rust-fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,54 @@
-use std::{
-    collections::HashSet,
-    convert::TryFrom,
-    io, io::prelude::*,
-    vec::Vec
-};
+use std::{collections::HashSet, convert::TryFrom, io, io::prelude::*, vec::Vec};
 
 use itertools::Itertools;
+use snafu::{ResultExt, Snafu};
 use unicode_segmentation::UnicodeSegmentation;
-use snafu::{Snafu, ResultExt};
-
 
 #[derive(Debug, Snafu)]
 pub enum IndexError {
-    UnableToReadPostingListSize { source: io::Error },
-    UnableToReadTermSize { term_id: u32, source: io::Error },
-    UnableToReadTerm { term_id: u32, source: io::Error },
-    UnableToReadNumberOfDocIds { term: String, term_id: u32, source: io::Error },
-    UnableToReadDocId { term: String, term_id: u32, doc_index: u32, source: io::Error },
-    UnableToReadNumberOfDocs { source: io::Error },
-    UnableToReadDocSize { doc_id: u32, source: io::Error },
-    UnableToReadDocContent { doc_id: u32, source: io::Error },
+    UnableToReadPostingListSize {
+        source: io::Error,
+    },
+    UnableToReadTermSize {
+        term_id: u32,
+        source: io::Error,
+    },
+    UnableToReadTerm {
+        term_id: u32,
+        source: io::Error,
+    },
+    UnableToReadNumberOfDocIds {
+        term: String,
+        term_id: u32,
+        source: io::Error,
+    },
+    UnableToReadDocId {
+        term: String,
+        term_id: u32,
+        doc_index: u32,
+        source: io::Error,
+    },
+    UnableToReadNumberOfDocs {
+        source: io::Error,
+    },
+    UnableToReadDocSize {
+        doc_id: u32,
+        source: io::Error,
+    },
+    UnableToReadDocContent {
+        doc_id: u32,
+        source: io::Error,
+    },
 }
 
-fn read_u32(reader: &mut impl io::BufRead) -> Result<u32, io::Error>
-{
+fn read_u32(reader: &mut impl io::BufRead) -> Result<u32, io::Error> {
     let mut buf = [0 as u8; 4];
     reader.read_exact(&mut buf)?;
 
     Ok(u32::from_be_bytes(buf))
 }
 
-fn read_u8(reader: &mut impl io::BufRead) -> Result<u8, io::Error>
-{
-    let mut buf = [0 as u8];
-    reader.read_exact(&mut buf)?;
-
-    Ok(buf[0])
-}
-
-fn read_u16(reader: &mut impl io::BufRead) -> Result<u16, io::Error>
-{
+fn read_u16(reader: &mut impl io::BufRead) -> Result<u16, io::Error> {
     let mut buf = [0 as u8; 2];
     reader.read_exact(&mut buf)?;
 
@@ -51,43 +60,46 @@ type PostingsList = HashMap<String, Vec<usize>>;
 
 #[derive(PartialEq, Debug)]
 pub struct Document {
-    pub content: String
+    pub content: String,
 }
 
 type TermList = HashSet<String>;
 
 pub struct AnalyzedDocument {
     terms: TermList,
-    content: String
+    content: String,
 }
 
 pub fn analyze(content: String) -> AnalyzedDocument {
-    let terms = content.unicode_words().map(|w| w.to_lowercase().to_string()).collect();
+    let terms = content
+        .unicode_words()
+        .map(|w| w.to_lowercase().to_string())
+        .collect();
     AnalyzedDocument { terms, content }
 }
 
-
 #[derive(Debug, PartialEq)]
-pub struct Index
-{
+pub struct Index {
     // Append only
     docs: Vec<Document>, // Doc ID comes from placement here
     // Terms in postings list are normalized (lowercased for now, more later)
     // TODO: Should probably templatize this later to allow variable numbers
     // but would mean that we need to increment our own counter rather
     // than using the vector size.
-    postings: PostingsList
+    postings: PostingsList,
 }
 
 impl Index {
     pub fn read<R>(reader: R) -> Result<Self, IndexError>
-        where R: io::Read
+    where
+        R: io::Read,
     {
         let mut reader = io::BufReader::new(reader);
 
         // First, postings size
         let num_terms = read_u32(&mut reader).context(UnableToReadPostingListSize)?;
-        let mut postings = PostingsList::with_capacity_and_hasher(num_terms as usize, Default::default());
+        let mut postings =
+            PostingsList::with_capacity_and_hasher(num_terms as usize, Default::default());
 
         for term_id in 0..num_terms {
             // Read the size of the term, then the term itself
@@ -96,16 +108,25 @@ impl Index {
             let mut term = String::new();
             {
                 let mut limited_reader = reader.by_ref().take(term_size as u64);
-                limited_reader.read_to_string(&mut term).context(UnableToReadTerm { term_id })?;
+                limited_reader
+                    .read_to_string(&mut term)
+                    .context(UnableToReadTerm { term_id })?;
             }
 
             // Then the number of doc ids and the doc ids themselves
-            let num_doc_ids = read_u32(&mut reader).context(UnableToReadNumberOfDocIds { term: &term, term_id })?;
+            let num_doc_ids = read_u32(&mut reader).context(UnableToReadNumberOfDocIds {
+                term: &term,
+                term_id,
+            })?;
 
             let mut doc_ids: Vec<usize> = Vec::with_capacity(num_doc_ids as usize);
 
             for doc_index in 0..num_doc_ids {
-                let doc_id = read_u32(&mut reader).context(UnableToReadDocId { term: &term, term_id, doc_index })?;
+                let doc_id = read_u32(&mut reader).context(UnableToReadDocId {
+                    term: &term,
+                    term_id,
+                    doc_index,
+                })?;
                 doc_ids.push(doc_id as usize);
             }
 
@@ -121,7 +142,9 @@ impl Index {
             let mut content = String::new();
             {
                 let mut limited_reader = reader.by_ref().take(content_size as u64);
-                limited_reader.read_to_string(&mut content).context(UnableToReadDocContent { doc_id })?;
+                limited_reader
+                    .read_to_string(&mut content)
+                    .context(UnableToReadDocContent { doc_id })?;
             }
 
             docs.push(Document { content })
@@ -131,7 +154,9 @@ impl Index {
     }
 
     pub fn search<'a>(&'a self, query: &str) -> Vec<&'a Document> {
-        query.to_lowercase().unicode_words()
+        query
+            .to_lowercase()
+            .unicode_words()
             .unique() // Only non-duplicate tokens
             .map(|tok| self.postings.get(tok))
             .filter(|option| option.is_some())
@@ -142,62 +167,110 @@ impl Index {
             .map(|doc_id| &self.docs[*doc_id])
             .collect()
     }
-
 }
-
 
 #[derive(Debug, Snafu)]
 pub enum IndexWriterError {
-    UnableToDownCastPostingsLength { len: usize, source: core::num::TryFromIntError },
-    UnableToWritePostingsLength { source: io::Error},
+    UnableToDownCastPostingsLength {
+        len: usize,
+        source: core::num::TryFromIntError,
+    },
+    UnableToWritePostingsLength {
+        source: io::Error,
+    },
 
-    UnableToDownCastTermLength { term: String, len: usize, source: core::num::TryFromIntError },
-    UnableToWriteTermLength { term: String, source: io::Error},
-    UnableToWriteTerm { term: String, source: io::Error},
+    UnableToDownCastTermLength {
+        term: String,
+        len: usize,
+        source: core::num::TryFromIntError,
+    },
+    UnableToWriteTermLength {
+        term: String,
+        source: io::Error,
+    },
+    UnableToWriteTerm {
+        term: String,
+        source: io::Error,
+    },
 
-    UnableToDownCastNumberOfDocIds { num_docs: usize, source: core::num::TryFromIntError },
-    UnableToWriteNumberOfDocIds { num_docs: u32, source: io::Error},
+    UnableToDownCastNumberOfDocIds {
+        num_docs: usize,
+        source: core::num::TryFromIntError,
+    },
+    UnableToWriteNumberOfDocIds {
+        num_docs: u32,
+        source: io::Error,
+    },
 
-    UnableToDownCastDocId { doc_id: usize, source: core::num::TryFromIntError },
-    UnableToWriteDocId { doc_id: u32, source: io::Error},
+    UnableToDownCastDocId {
+        doc_id: usize,
+        source: core::num::TryFromIntError,
+    },
+    UnableToWriteDocId {
+        doc_id: u32,
+        source: io::Error,
+    },
 
-    UnableToDownCastNumberOfDocs { num_docs: usize, source: core::num::TryFromIntError },
-    UnableToWriteNumberOfDocs { num_docs: u32, source: io::Error},
+    UnableToDownCastNumberOfDocs {
+        num_docs: usize,
+        source: core::num::TryFromIntError,
+    },
+    UnableToWriteNumberOfDocs {
+        num_docs: u32,
+        source: io::Error,
+    },
 
-    UnableToDownCastDocLength { content_len: usize, source: core::num::TryFromIntError },
-    UnableToWriteDocLength { content_len: u32, source: io::Error},
+    UnableToDownCastDocLength {
+        content_len: usize,
+        source: core::num::TryFromIntError,
+    },
+    UnableToWriteDocLength {
+        content_len: u32,
+        source: io::Error,
+    },
     // TODO: include path?
-    UnableToWriteDoc { source: io::Error },
+    UnableToWriteDoc {
+        source: io::Error,
+    },
 
-    UnableToFlush { source: io::Error },
+    UnableToFlush {
+        source: io::Error,
+    },
 }
-
 
 #[derive(PartialEq, Debug, Default)]
 pub struct IndexWriter {
     docs: Vec<Document>,
-    postings: PostingsList
+    postings: PostingsList,
 }
 
 impl From<IndexWriter> for Index {
     fn from(writer: IndexWriter) -> Self {
-        Index { docs: writer.docs, postings: writer.postings }
+        Index {
+            docs: writer.docs,
+            postings: writer.postings,
+        }
     }
 }
-
 
 impl IndexWriter {
     pub fn add(&mut self, doc: AnalyzedDocument) {
         let doc_id = self.docs.len();
         for term in doc.terms {
-            (self.postings.entry(term.to_string()).or_insert_with(Vec::new))
-                .push(doc_id);
+            (self
+                .postings
+                .entry(term.to_string())
+                .or_insert_with(Vec::new))
+            .push(doc_id);
         }
-        self.docs.push(Document { content: doc.content.to_string() });
+        self.docs.push(Document {
+            content: doc.content.to_string(),
+        });
     }
 
     pub fn write<W>(&self, writer: W) -> Result<(), IndexWriterError>
-        where W : io::Write
+    where
+        W: io::Write,
     {
         let mut writer = io::BufWriter::new(writer);
         // Write postings
@@ -212,27 +285,42 @@ impl IndexWriter {
         //
         // POSTINGS_SIZE:u32 [TERM_SIZE:u8 TERM NUM_DOC_IDS: u32 [u32, u32]], ...
         let postings_len = self.postings.keys().len();
-        let postings_len = u32::try_from(postings_len).context(UnableToDownCastPostingsLength { len: postings_len })?;
-        writer.write_all(&postings_len.to_be_bytes()).context(UnableToWritePostingsLength)?;
+        let postings_len = u32::try_from(postings_len)
+            .context(UnableToDownCastPostingsLength { len: postings_len })?;
+        writer
+            .write_all(&postings_len.to_be_bytes())
+            .context(UnableToWritePostingsLength)?;
 
         for (term, doc_ids) in &self.postings {
             // Term length, then term
             let term_bytes = term.as_bytes();
             let term_length = term_bytes.len();
-            let term_length =
-                u16::try_from(term_length).context(UnableToDownCastTermLength { term: term, len: term_length })?;
+            let term_length = u16::try_from(term_length).context(UnableToDownCastTermLength {
+                term: term,
+                len: term_length,
+            })?;
 
-            writer.write_all(&term_length.to_be_bytes()).context(UnableToWriteTermLength { term: term })?;
-            writer.write_all(&term_bytes[..]).context(UnableToWriteTerm { term: term })?;
+            writer
+                .write_all(&term_length.to_be_bytes())
+                .context(UnableToWriteTermLength { term: term })?;
+            writer
+                .write_all(&term_bytes[..])
+                .context(UnableToWriteTerm { term: term })?;
 
             // Number of docs, then the docs
             let num_docs = doc_ids.len();
-            let num_docs = u32::try_from(num_docs).context(UnableToDownCastNumberOfDocIds { num_docs })?;
-            writer.write_all(&num_docs.to_be_bytes()).context(UnableToWriteNumberOfDocIds { num_docs })?;
+            let num_docs =
+                u32::try_from(num_docs).context(UnableToDownCastNumberOfDocIds { num_docs })?;
+            writer
+                .write_all(&num_docs.to_be_bytes())
+                .context(UnableToWriteNumberOfDocIds { num_docs })?;
 
             for doc_id in doc_ids {
-                let doc_id = u32::try_from(*doc_id).context(UnableToDownCastDocId { doc_id: *doc_id })?;
-                writer.write_all(&doc_id.to_be_bytes()).context(UnableToWriteDocId { doc_id })?;
+                let doc_id =
+                    u32::try_from(*doc_id).context(UnableToDownCastDocId { doc_id: *doc_id })?;
+                writer
+                    .write_all(&doc_id.to_be_bytes())
+                    .context(UnableToWriteDocId { doc_id })?;
             }
         }
 
@@ -240,21 +328,28 @@ impl IndexWriter {
         //
         // Number of documents, then doc length and content pairs
         let num_docs = self.docs.len();
-        let num_docs = u32::try_from(num_docs).context(UnableToDownCastNumberOfDocs { num_docs })?;
-        writer.write_all(&num_docs.to_be_bytes()).context(UnableToWriteNumberOfDocs { num_docs })?;
+        let num_docs =
+            u32::try_from(num_docs).context(UnableToDownCastNumberOfDocs { num_docs })?;
+        writer
+            .write_all(&num_docs.to_be_bytes())
+            .context(UnableToWriteNumberOfDocs { num_docs })?;
 
         for doc in &self.docs {
             let content_len = doc.content.len();
-            let content_len = u32::try_from(content_len).context(UnableToDownCastDocLength { content_len })?;
-            writer.write_all(&content_len.to_be_bytes()).context(UnableToWriteDocLength { content_len })?;
-            writer.write_all(doc.content.as_bytes()).context(UnableToWriteDoc)?;
+            let content_len =
+                u32::try_from(content_len).context(UnableToDownCastDocLength { content_len })?;
+            writer
+                .write_all(&content_len.to_be_bytes())
+                .context(UnableToWriteDocLength { content_len })?;
+            writer
+                .write_all(doc.content.as_bytes())
+                .context(UnableToWriteDoc)?;
         }
 
         writer.flush().context(UnableToFlush)?;
 
         Ok(())
     }
-
 }
 
 #[cfg(test)]
@@ -262,7 +357,6 @@ mod tests {
     use super::*;
 
     // Index tests
-
 
     #[test]
     fn add_to_index() {
@@ -287,10 +381,11 @@ mod tests {
         idx.add(cats_better);
         idx.add(analyze("no".to_string()));
 
-        let results: Vec<String> =
-            Index::from(idx).search("cats").iter()
-                .map(|d| d.content.clone())
-                .collect();
+        let results: Vec<String> = Index::from(idx)
+            .search("cats")
+            .iter()
+            .map(|d| d.content.clone())
+            .collect();
         assert_eq!(expected, results);
     }
 
@@ -317,16 +412,18 @@ mod tests {
         let mut buf = io::Cursor::new(vec![]);
         index.write(&mut buf)?;
 
-        assert_eq!(&[0, 0, 0, 1,        // One posting
-                     3,                 // Three letters
-                     b'f', b'o', b'o',
-                     0, 0, 0, 1,        // One doc_id
-                     0, 0, 0, 0,        // Doc 0
-                     0, 0, 0, 1,        // One doc
-                     0, 0, 0, 3,        // Length of first doc
-                     b'f', b'o', b'o'   // The doc content
-                    ],
-                   &buf.get_ref()[..]);
+        assert_eq!(
+            &[
+                0, 0, 0, 1, // One posting
+                0, 3, // Three letters
+                b'f', b'o', b'o', 0, 0, 0, 1, // One doc_id
+                0, 0, 0, 0, // Doc 0
+                0, 0, 0, 1, // One doc
+                0, 0, 0, 3, // Length of first doc
+                b'f', b'o', b'o' // The doc content
+            ],
+            &buf.get_ref()[..]
+        );
 
         Ok(())
     }
@@ -336,7 +433,8 @@ mod tests {
         let buf =
             // One term in the postings list: foo
             [0, 0, 0, 1,
-                3, b'f', b'o', b'o',
+                0, 3,
+                b'f', b'o', b'o',
             // To one doc, doc_id 0
                 0, 0, 0, 1, 0, 0, 0, 0,
             // One stored doc, of length 3
